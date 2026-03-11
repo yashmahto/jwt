da1023a0fdaec3c7a76ce0d312d1d053a9660cfd
Yash Mahto <yashmahto244@gmail.com>
2026-03-11 18:40:37 +0530

    add controllers

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
   res.send("JWT Authentication ");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
 
